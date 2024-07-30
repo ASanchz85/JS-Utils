afb1f261bdc3fb721f2d8c3a4eea0191bc8f807d
Antonio Sánchez <asanchz85@gmail.com>
2024-07-30 19:48:06 +0200

    Create deepObjectComparator.ts

---
+++ deepObjectComparator.ts
@@ -0,0 +1,43 @@
+export const deepEqual = <T>(obj1: T, obj2: T): boolean => {
+  if (obj1 === obj2) {
+    return true
+  }
+
+  if (
+    typeof obj1 !== 'object' ||
+    obj1 === null ||
+    typeof obj2 !== 'object' ||
+    obj2 === null
+  ) {
+    return false
+  }
+
+  const keys1 = Object.keys(obj1) as Array<keyof T>
+  const keys2 = Object.keys(obj2) as Array<keyof T>
+
+  if (keys1.length !== keys2.length) {
+    return false
+  }
+
+  for (const key of keys1) {
+    if (!keys2.includes(key)) {
+      return false
+    }
+
+    const val1 = obj1[key]
+    const val2 = obj2[key]
+
+    const areObjects = isObject(val1) && isObject(val2)
+    if (
+      (areObjects && !deepEqual(val1, val2)) ||
+      (!areObjects && val1 !== val2)
+    ) {
+      return false
+    }
+  }
+
+  return true
+}
+
+const isObject = (obj: unknown): obj is object =>
+  obj !== null && typeof obj === 'object'
