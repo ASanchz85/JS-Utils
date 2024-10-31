34e3903b4a7943cf30da5d21d480a74cd9444ad2
Antonio Sánchez <asanchz85@gmail.com>
2024-10-31 12:57:04 +0100

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-// updated to '24 october - typescript
+// updated at 31th october 2024
 
 import globals from 'globals'
 import pluginJs from '@eslint/js'
@@ -6,7 +6,12 @@ import tseslint from 'typescript-eslint'
 import pluginReact from 'eslint-plugin-react'
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  {
+    ignores: ['node_modules/', 'dist/']
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}']
+  },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
@@ -25,14 +30,21 @@ export default [
       quotes: [
         'error',
         'single',
-        { avoidEscape: true, allowTemplateLiterals: true }
+        {
+          avoidEscape: true,
+          allowTemplateLiterals: true
+        }
       ],
       semi: ['error', 'never'],
-      'no-unused-vars': [
+      '@typescript-eslint/no-unused-vars': [
         'error',
-        { vars: 'all', args: 'after-used', ignoreRestSiblings: true }
+        {
+          vars: 'all',
+          args: 'after-used',
+          ignoreRestSiblings: true
+        }
       ],
-      'no-redeclare': 'error',
+      '@typescript-eslint/no-redeclare': 'error',
       eqeqeq: ['error', 'always'],
       'comma-dangle': ['error', 'never'],
       'space-before-function-paren': [
@@ -59,8 +71,8 @@ export default [
       'no-loop-func': 'error',
       'function-paren-newline': ['error', 'consistent'],
 
-      'react/react-in-jsx-scope': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off'
+      // Custom rules
+      'react/react-in-jsx-scope': 'off'
     }
   }
 ]
