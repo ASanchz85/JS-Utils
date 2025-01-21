ebf97e892f068bf49da2a493ad987469af495813
Antonio Sánchez <asanchz85@gmail.com>
2025-01-21 10:57:18 +0100

    Update domHandlers.ts

--- DOMUtils/domHandlers.ts
+++ DOMUtils/domHandlers.ts
@@ -1,11 +1,30 @@
 export const removeDivsWithClassName = (className: string) => {
   const elements = document.querySelectorAll<HTMLDivElement>(className)
   if (!elements || elements.length === 0) {
-    console.warn('Element to remove not found.')
+    console.warn('Element to remove not found or already unmounted.')
     return
   }
 
-  elements.forEach((element) => (element.style.display = 'none'))
+  elements.forEach((element) => {
+    try {
+      if (element.parentNode) {
+        element.parentNode.removeChild(element)
+      }
+    } catch (error) {
+      console.warn('Element parent not found. Skipping removal.')
+    }
+  })
+}
+
+export const toggleVisibility = (className: string) => {
+  const elements = document.querySelectorAll<HTMLDivElement>(className)
+  if (!elements || elements.length === 0) {
+    return
+  }
+
+  elements.forEach((element) => {
+    element.classList.add('hidden')
+  })
 }
 
 export const getHtmlElementSize = (className: string) => {
@@ -17,4 +36,15 @@ export const getHtmlElementSize = (className: string) => {
   }
 
   return element.getBoundingClientRect()
+}
+
+export const removeClass = (className: string, classToRemove: string) => {
+  const elements = document.querySelectorAll<HTMLDivElement>(className)
+  if (!elements || elements.length === 0) {
+    return
+  }
+
+  elements.forEach((element) => {
+    element.classList.remove(classToRemove)
+  })
 }
